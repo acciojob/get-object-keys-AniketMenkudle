8de158922bd2f83e9f26393c60403bf3812ce533
AniketMenkudle <aniketmenkudle94@gmail.com>
2026-03-28 17:14:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 //your JS code here. If required.
 const student = {
-	name: "Aniket",
-	age: 22,
-	city:"pune"
+  name: "Aniket",
+  age: 22,
+  city: "pune"
 };
 
 function getKeys(obj){
-	return Object.keys(obj);
+  return Object.keys(obj);
 }
 
 console.log(getKeys(student));
